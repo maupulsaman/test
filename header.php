0153d16d12146a791d81a5383702e389e00620de
John Doe <johndoe@example.com>
2016-10-23 22:09:08 +0530

    first commit

---
+++ header.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<!--[if IE 7]>
+
+<html class="ie ie7" <?php language_attributes(); ?>>
+
+<![endif]-->
+
+<!--[if IE 8]>
+
+<html class="ie ie8" <?php language_attributes(); ?>>
+
+<![endif]-->
+
+<!--[if !(IE 7) & !(IE 8)]><!-->
+
+<html <?php language_attributes(); ?>>
+
+<!--<![endif]-->
+<head>
+    
+    <title><?php wp_title(); ?></title>
+    <meta name="format-detection" content="telephone=no"/>
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, 
+    user-scalable=0"/>
+    <meta name="description" content="We at Kethaka Walawwa (Private) Elder Care Home are qualified medical professionals with lot of experience in the field of aged care">
+    <meta name="keywords" content="elder care sri lanka, elderly care homes in sri lanka ,nursing home sri lanka,eldercare, nursing home sri lanka ,elders home,Health Care, Elders Care Home, Sri Lanka, Elders Nursing Home">
+   
+    <link href='//fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>   
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
+    <link href="<?php echo get_site_url(); ?>/wp-content/themes/eldercare/css/owl.carousel.css" rel="stylesheet">
+    <link href="<?php echo get_site_url(); ?>/wp-content/themes/eldercare/css/owl.theme.css" rel="stylesheet">
+    <link href="<?php echo get_site_url(); ?>/wp-content/themes/eldercare/css/owl.transitions.css" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo get_site_url(); ?>/wp-content/themes/eldercare/css/animate.min.css">
+    <link rel="stylesheet" href="<?php echo get_site_url(); ?>/wp-content/themes/eldercare/style.css">
+
+
+    <style>
+    
+    
+</style>
+
+ <?php wp_head(); ?>  
+ <style type="text/css">
+ .contact-no{
+  position: relative;
+  font-weight: bold;
+ }
+ .nav>li>a{
+  padding: 10px 12px ;
+ }
+ .language{
+    line-height: 90px;
+    font-size: 16px;
+ }
+ </style>
+</head>
+
+<body class="appear-animate">
+<div id="preloader">
+    <div id="status">&nbsp;</div>
+</div>
+<!-- Page Loader       
+<div class="page-loader">
+<img src="images/logo.png" style="display: block; margin-left: 10px;">
+<div><img style="display: block; width:100%;" src="images/slide1.jpg"></div>
+<div class="loader"></div>
+</div>
+<!-- End Page Loader -->
+
+
+<div class="container-fluid nav-header">
+    <div class="row">
+        <div class="col-lg-2 col-md-2 col-xs-12 col-sm-4">
+          <a href="<?php echo get_site_url(); ?>"> <img src="<?php echo get_site_url(); ?>/wp-content/themes/eldercare/images/logo.jpg" > </a>
+        </div>
+        <div class="col-lg-6 col-md-7 col-xs-12 col-sm-8">
+             <!-- Static navbar -->
+      <nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            
+          </div>
+          <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="http://eldercaresrilanka.com">HOME</a></li>
+              <li><a href="http://eldercaresrilanka.com/about-us/">ABOUT US</a></li>
+              <li><a href="http://eldercaresrilanka.com/services-in-kethaka-walawwa/">SERVICES</a></li>
+              <li><a href="http://eldercaresrilanka.com/facilities-in-kethaka-walawwa/">FACILITIES</a></li>
+              <li><a href="http://eldercaresrilanka.com/gallery/">GALLERY</a></li>
+              <li><a href="http://eldercaresrilanka.com/contact-us/">CONTACT US</a></li>
+              
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div><!--/.container-fluid -->
+      </nav>
+
+        </div>
+        <div class="col-lg-2 col-md-2 col-xs-6 col-sm-3 contact-no">
+        
+        <i class="fa fa-phone" aria-hidden="true"></i>+94 777 277 377</div>
+          <div class="col-lg-2 col-md-2 col-xs-6 col-sm-3 language">
+          <a href="http://eldercaresrilanka.com/">English</a> | <a href="http://eldercaresrilanka.com/sin">සිංහල</a>
+          </div>
+        <!--<div class="col-lg-1 col-md-1 social"><i class="fa fa-facebook" aria-hidden="true"></i></div>-->
+    </div>
+</div>
+        
